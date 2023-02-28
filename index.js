6539f73fef40133423b858481dcdc94f1246167d
Natalie Wolfe <natalie@lifewanted.com>
2023-02-27 21:43:12 -0800

    Fix lift command

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ async function getMetadata() {
 
 async function setCommands() {
   await bot.setMyCommands([{
-    command: "\\mylifts",
+    command: "/mylifts",
     description: "LIFT MORE"
   }]);
 }
@@ -250,7 +250,6 @@ bot.onText(
         const response = `YA GONNA GET SWOLE DOING ${match.groups.lift.toUpperCase()}?\r\nSETS: ${match.groups.sets}\r\nREPS: ${match.groups.reps}\r\nWEIGHT: ${match.groups.weight}`;
         bot.sendMessage(chatId, response);
       }
-
     } catch(error) {
       // console.log('Lift post error',error)
       console.log('message:', error.message)
@@ -266,13 +265,11 @@ bot.onText(
       bot.sendMessage(chatId, 'FUCK YOU WEAKLING')
       }
     }
-
-
   }
 );
 
 // TODO: fix "lift" regex, spit errors on failed posts, fix async bot msging
-bot.onText(/^\\?my ?lifts$/,
+bot.onText(/^\/?my ?lifts(?:@scooper_bot)?$/,
   async (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
